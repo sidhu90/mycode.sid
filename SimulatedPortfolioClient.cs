ce71f81234e658d6ec781bf4ed34e298e96d110f
sidhu90 <cloudops.sid@gmail.com>
2018-06-01 11:42:11 -0500

    added test file

---
+++ SimulatedPortfolioClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Apollo.Api.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Apollo.Client
+{
+    public interface ISimulatedPortfolioClient
+    {
+        [Obsolete(Constants.ObsoleteMessage)]
+        IList<SimPortfolioSetup> GetAllBySimType(string simType);
+    }
+
+    public class SimulatedPortfolioClient : ClientCache, ISimulatedPortfolioClient
+    {
+        public SimulatedPortfolioClient(IClientRequestor clientRequestor)
+            : base("/SimulatedPortfolio/", clientRequestor)
+        {
+        }
+
+        [Obsolete(Constants.ObsoleteMessage)]
+        public IList<SimPortfolioSetup> GetAllBySimType(string simType)
+        {
+            return Get<IList<SimPortfolioSetup>>($"GetAll/{simType}");
+        }
+    }
+}
